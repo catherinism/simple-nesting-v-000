383fe126c693bfe7859104285ddedc6ec69170b1
Catherine Salazar <catcreates13@gmail.com>
2018-01-28 22:20:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,13 +84,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-			  :yukihiro_matsumoto => {
-				   :known_for => "Ruby",
-				    :languages => ["LISP", "C"]
-				 }
-     }
-
-		 programmer_hash
+			}
 
 end
 
